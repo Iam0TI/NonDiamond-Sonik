73f1495b752e03746761ef62f56f9ea63eb7c380
youngancient <tochyokoye@gmail.com>
2024-11-20 18:00:46 +0100

    added tests

--- test/deployDiamond.t.sol
+++ test/deployDiamond.t.sol
@@ -10,13 +10,16 @@ import {SonikDrop} from "../contracts/facets/erc20facets/SonikDropFacet.sol";
 import "../contracts/Diamond.sol";
 
 import "./helpers/DiamondUtils.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 100000e18);
     }
+
+    // function mint(address to, uint256 amount) public {
+    //     _mint(to, amount);
+    // }
 }
 
 contract DiamondDeployer is DiamondUtils, IDiamondCut {
@@ -28,11 +31,12 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
     AirdropFactoryFacet factoryF;
     SonikDrop sonikDropF;
 
+    TestERC20 shibuyaToken;
+
     address owner = makeAddr("youngancient");
 
     function setUp() public {
         //deploy facets
-        
 
         dCutFacet = new DiamondCutFacet();
         diamond = new Diamond(address(this), address(dCutFacet));
@@ -75,16 +79,61 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
         //call a function
         DiamondLoupeFacet(address(diamond)).facetAddresses();
 
+        // deploy test erc20
+        vm.prank(owner);
+        shibuyaToken = new TestERC20("ShibuyaToken", "SHIB");
+
+        assertEq(shibuyaToken.balanceOf(owner), 100000e18);
 
         //interact with factory
-        address[] memory addresses = AirdropFactoryFacet(address(diamond)).getAllSonikDropClones();
-        assertEq(addresses.length, 0 );
+        address[] memory addresses = AirdropFactoryFacet(address(diamond))
+            .getAllSonikDropClones();
+        assertEq(addresses.length, 0);
 
         // create sonik token drop without NFT
-        address _tokenAddress = address(0x01);
+        address _tokenAddress = address(shibuyaToken);
         bytes32 _merkleRoot = 0x29c08bc8bf7d3a0ed4b1dd16063389608cf9dec220f1584e32d317c2041e1fa4;
         uint256 _noOfClaimers = 100;
-        uint256 _totalOutputTokens = 1000;
+        uint256 _totalOutputTokens = 10000e18;
+
+        // should revert without tokenApproval
+        vm.expectRevert();
+        AirdropFactoryFacet(
+            address(diamond)
+        ).createSonikDrop(
+                _tokenAddress,
+                _merkleRoot,
+                _noOfClaimers,
+                _totalOutputTokens
+            );
+        
+        // approve token
+        vm.startPrank(owner);
+        shibuyaToken.approve(address(diamond), 50000e18);
+
+        assertEq(shibuyaToken.allowance(owner, address(diamond)),50000e18);
+
+        // create sonik contract
+         LibDiamond.SonikDropObj memory sonikDropObj = AirdropFactoryFacet(
+            address(diamond)
+        ).createSonikDrop(
+                _tokenAddress,
+                _merkleRoot,
+                _noOfClaimers,
+                _totalOutputTokens
+            );
+        assertEq(sonikDropObj.nftAddress, address(0) );
+        assertEq(sonikDropObj.tokenAddress, _tokenAddress);
+        assertEq(sonikDropObj.owner, owner);
+        assertEq(sonikDropObj.merkleRoot, _merkleRoot);
+
+        // test claiming
+        assertEq(shibuyaToken.balanceOf(sonikDropObj.contractAddress),_totalOutputTokens);
+
+        // should we make the sonikFacet depend on diamond or have it's own storage
+
+        
+
     }
 
     function testCreateSonikDrop() public {
